0cec4227a35b4b966d906ec97eba7892bcc1b90e
julesSAWAdev <mugisha560@gmail.com>
2021-05-04 11:32:04 +0200

    up to date may

--- web_dash/footer.php
+++
@@ -1,8 +0,0 @@
- <div class="footer">
-                <div class="float-right">
-                    La Corniche Motel <strong></strong> 
-                </div>
-                <div>
-                    <strong>Copyright</strong> Reserved &copy; 2020
-                </div>
-            </div>
